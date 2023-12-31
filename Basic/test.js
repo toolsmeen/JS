64beca2946c46fc6713b272ed361ff740965d392
Satnam Marday <83498275+toolsmeen@users.noreply.github.com>
2023-12-31 08:15:54 +0000

    test file me basic code done

--- Basic/test.js
+++ Basic/test.js
@@ -1,1 +1,12 @@
-console.log("satnam");
+const accountId =  222333
+let accountEmail = "Admin@toolsmeen.com"
+var accountPass = "Admin@123"
+accountCity = "Moonak"
+let accountState;
+
+// accountId= 2
+accountEmail = "admin@google.com"
+accountPass = "SatNam@123"
+accountCity = "Patiala"
+
+console.table([accountId, accountEmail, accountPass, accountCity, accountState]);
