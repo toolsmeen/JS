b73d574257db8aa5da600fd5618a810139252dfd
Satnam Marday <83498275+toolsmeen@users.noreply.github.com>
2024-01-01 13:31:33 +0000

    string kafi main topic ha jayda dyan dena work saved

---
+++ Basic/string.js
@@ -0,0 +1,29 @@
+let name = "Satnam singh"
+let age = 25
+
+
+// console.log(name +  age +  " value")   this  type of syntax not used nowadays
+
+// console.log(`Hello ${name} maine suna ha apki age ${age } hai `);
+
+
+let gameName = new String("tools-meen")  
+
+// console.log(gameName.indexOf('e'));
+
+const newString = gameName.substring(0,4);
+// console.log(newString);
+
+const anotherString = gameName.trim()
+// console.log(anotherString);
+
+const yoString = "    satnam    "
+console.log(yoString);
+console.log(yoString.bold);``
+
+const url = "https://toolsmeen.com/blog%20singh"
+console.log(url.replace('%20', '-'))
+
+
+
+// String is the main topic in JS bhai ache se program bna kar dekh lena kafi jayda main topic  ha 
