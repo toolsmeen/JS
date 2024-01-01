9941a8f35af63414f84c677fa4714c208a19a4a7
Satnam Marday <83498275+toolsmeen@users.noreply.github.com>
2024-01-01 12:36:03 +0000

    save

--- Basic/conversion.js
+++ Basic/conversion.js
@@ -3,5 +3,9 @@ let value= "true"
 // console.log(typeof value)
 
 let check = Number(value);  
-console.log(typeof check);
-console.log(check);
+// console.log(typeof check);
+// console.log(check);
+
+console.log(+true);
+console.log(+false);
+
