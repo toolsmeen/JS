d06e0b220f832dba343016bd90cf9e7b01b49363
Satnam Marday <83498275+toolsmeen@users.noreply.github.com>
2024-01-01 12:33:33 +0000

    memory krdi done  easy c

---
+++ Basic/memory.js
@@ -0,0 +1,23 @@
+// let myYtName = "Satnam Marda"
+
+// let myIgName= myYtName
+
+// myIgName = "Suraj Singh"
+
+// console.log(myYtName);
+// console.log(myIgName);
+
+
+let  userOne = {
+    email: "admin@toolsmeen.com",
+    UPI: "Toolsmeen@upi"
+}
+
+let userTwo = userOne
+
+userTwo.email = "satnam@gmail.com"
+userTwo.UPI = "Chal Nikal Tu"
+console.log(userOne)
+console.log(userTwo)
+
+
