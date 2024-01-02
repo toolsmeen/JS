6e5727807ae23a09ad4ce12484394ecacc08b335
Satnam Marday <83498275+toolsmeen@users.noreply.github.com>
2024-01-02 18:42:25 +0000

    kuj  ke code try kita bs

---
+++ Basic/number.js
@@ -0,0 +1,18 @@
+// // its all about numbers 
+
+// const age = 300
+//  console.log(age);
+//  const balance = new Number(400)
+//  console.log(balance);
+
+
+//  const otherNumber = 23332
+//  console.log(otherNumber.toLocaleString().length);
+
+
+
+
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Math started 
+
+console.log(Math.max(54,34,3,2,43,2,43,434))
